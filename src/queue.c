d8cd816508f5b3e1e951b242f43581569b660ce6
adamsvystun <adam.svystun@gmail.com>
2018-12-25 01:59:48 +0100

    Add message queues

---
+++ src/queue.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#include "queue.h"
+
+int mutex_lock(int fd) {
+    int qid = msgget(SFS_QUEUE_KEY, 0666);
+    if(qid < 0) {
+        return SFSQ_MSGGET_ERROR;
+    }
+    sfs_msg message;
+    ssize_t ret = msgrcv(qid, &message, sizeof(message), fd, MSG_NOERROR);
+    if(ret < 0) {
+        return SFSQ_MSGRCV_ERROR;
+    }
+    return SFSQ_OK;
+}
+
+int mutex_unlock(int fd) {
+    int qid = msgget(SFS_QUEUE_KEY, 0666);
+    if(qid < 0) {
+        return SFSQ_MSGGET_ERROR;
+    }
+    sfs_msg message;
+    message.fd = fd;
+    ssize_t ret = msgsnd(qid, &message, sizeof(message), MSG_NOERROR);
+    if(ret < 0) {
+        return SFSQ_MSGSND_ERROR;
+    }
+    return SFSQ_OK;
+}
+
+int queue_init() {
+    //@TODO
+}
